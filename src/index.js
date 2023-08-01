8253eb66d6113adbb70b7efd76292bf9546933d4
yansenvic <yansenvic@gmail.com>
2023-08-01 11:30:33 +0700

    refactor : form implement reusable component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, useField, Form } from "formik";
 import * as Yup from "yup";
 import "./styles.css";
 
@@ -59,40 +59,170 @@ import "./styles.css";
 // untuk mempercepat dan mempermudah pemahamaan kode, konsepnya sama tetap menggunakan useformik tetapi useformik ini sudah dibungkus dalam komponen Formik
 // dengan menggunakan Field, ErrorMessage dan Form maka dapat mempersingkat pembuatan form dengan mengurangi input dengan Field, error dengan MessageError dan form dengan Form
 
+// const SignupForm = () => {
+//   return (
+//     <Formik
+//       initialValues={{ firstName: "", lastName: "", email: "" }}
+//       validationSchema={Yup.object({
+//         firstName: Yup.string()
+//           .max(15, "Must be 15 characters or less")
+//           .required("Required"),
+//         lastName: Yup.string()
+//           .max(20, "Must be 20 characters or less")
+//           .required("Required"),
+//         email: Yup.string().email("Invalid email address").required("Required"),
+//       })}
+//       onSubmit={(values, { setSubmitting }) => {
+//         setTimeout(() => {
+//           alert(JSON.stringify(values, null, 2));
+//           setSubmitting(false);
+//         }, 400);
+//       }}
+//     >
+//       <Form>
+//         <label htmlFor="firstName">First Name</label>
+//         <Field name="firstName" type="text" />
+//         <ErrorMessage name="firstName" />
+//         <label htmlFor="lastName">Last Name</label>
+//         <Field name="lastName" type="text" />
+//         <ErrorMessage name="lastName" />
+//         <label htmlFor="email">Email Address</label>
+//         <Field name="email" type="email" />
+//         <ErrorMessage name="email" />
+//         <br />
+//         <button type="submit">Submit</button>
+//       </Form>
+//     </Formik>
+//   );
+// };
+
+//ini adalah bentuk terakhir dari formik dengan memaksimalkan reusable komponen pada MyTextInput dllnya.
+const MyTextInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+  // React treats radios and checkbox inputs differently from other input types: select and textarea.
+  // Formik does this too! When you specify `type` to useField(), it will
+  // return the correct bag of props for you -- a `checked` prop will be included
+  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <div>
+      <label className="checkbox-input">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+// And now we can use these
 const SignupForm = () => {
   return (
-    <Formik
-      initialValues={{ firstName: "", lastName: "", email: "" }}
-      validationSchema={Yup.object({
-        firstName: Yup.string()
-          .max(15, "Must be 15 characters or less")
-          .required("Required"),
-        lastName: Yup.string()
-          .max(20, "Must be 20 characters or less")
-          .required("Required"),
-        email: Yup.string().email("Invalid email address").required("Required"),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" type="text" />
-        <ErrorMessage name="firstName" />
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" type="text" />
-        <ErrorMessage name="lastName" />
-        <label htmlFor="email">Email Address</label>
-        <Field name="email" type="email" />
-        <ErrorMessage name="email" />
-        <br />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+    <>
+      <h1>Subscribe!</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          acceptedTerms: false, // added for our checkbox
+          jobType: "", // added for our select
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          lastName: Yup.string()
+            .max(20, "Must be 20 characters or less")
+            .required("Required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          acceptedTerms: Yup.boolean()
+            .required("Required")
+            .oneOf([true], "You must accept the terms and conditions."),
+          jobType: Yup.string()
+            .oneOf(
+              ["designer", "development", "product", "other"],
+              "Invalid Job Type"
+            )
+            .required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="First Name"
+            name="firstName"
+            type="text"
+            placeholder="Jane"
+          />
+
+          <MyTextInput
+            label="Last Name"
+            name="lastName"
+            type="text"
+            placeholder="Doe"
+          />
+
+          <MyTextInput
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="jane@formik.com"
+          />
+
+          <MySelect label="Job Type" name="jobType">
+            <option value="">Select a job type</option>
+            <option value="designer">Designer</option>
+            <option value="development">Developer</option>
+            <option value="product">Product Manager</option>
+            <option value="other">Other</option>
+          </MySelect>
+
+          <MyCheckbox name="acceptedTerms">
+            I accept the terms and conditions
+          </MyCheckbox>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
